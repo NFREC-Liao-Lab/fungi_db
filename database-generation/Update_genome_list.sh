e9c821284ade68d96ab1d47deafdd4801996824c
Haihua <58599570+HaihuaWang-hub@users.noreply.github.com>
2022-06-01 18:54:19 -0400

    Update Update_genome_list.sh

--- database-generation/Update_genome_list.sh
+++ database-generation/Update_genome_list.sh
@@ -1,1 +1,12 @@
 #The scripts for update the FunDB genome list
+# Part 1
+# cd ~/data_storage/SATA1/FunDB
+# python ~/data_storage/SATA1/FunDB_git/fungi_db/database-generation/process_JGI_genome_web.py
+# By run the above command, a file named "FunDB_genomes_info_FungalTraits_Year_Month_Date.tsv" and "FunDB_genomes_info_FungalTraits_Last_Update.tsv" will receive.
+
+#Part2 check list for update
+
+
+
+
+    
