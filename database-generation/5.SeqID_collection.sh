163294b2316ef66978378f2b4f649ce4a346aa77
Haihua <58599570+HaihuaWang-hub@users.noreply.github.com>
2022-06-02 00:20:57 -0400

    Update 5.SeqID_collection.sh

--- database-generation/5.SeqID_collection.sh
+++ database-generation/5.SeqID_collection.sh
@@ -1,1 +1,17 @@
+#Part6 collect the database sequence id
 
+#FunDB_prot_dir=/home/microbiome/data_storage/SATA1/Database/prot
+#FunDB_nucl_dir=/home/microbiome/data_storage/SATA1/Database/nucl
+FunDB_dir=/home/microbiome/data_storage/SATA1/Database
+
+echo "SeqID" > $FunDB_dir/FunDB_prot_seqID.tmp.tsv
+for seq in $(ls $FunDB_dir/prot); do
+    cat $FunDB_dir/prot/$seq |seqkit seq -n >> $FunDB_dir/FunDB_prot_seqID.tmp.tsv
+done
+mv -f $FunDB_dir/FunDB_prot_seqID.tmp.tsv $FunDB_dir/FunDB_prot_seqID.tsv
+
+echo "SeqID" > $FunDB_dir/FunDB_nucl_seqID.tmp.tsv
+for seq in $(ls $FunDB_dir/nucl); do
+    cat $FunDB_dir/nucl/$seq |seqkit seq -n >> $FunDB_dir/FunDB_nucl_seqID.tmp.tsv
+done
+mv -f $FunDB_dir/FunDB_nucl_seqID.tmp.tsv $FunDB_dir/FunDB_nucl_seqID.tsv
