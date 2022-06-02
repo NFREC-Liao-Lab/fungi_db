7f901b9a87b5d5233e6b03ee8c04ca14e0f4c923
Haihua <58599570+HaihuaWang-hub@users.noreply.github.com>
2022-06-01 23:47:18 -0400

    Update 4.blast_result_process.sh

--- database-generation/4.blast_result_process.sh
+++ database-generation/4.blast_result_process.sh
@@ -11,14 +11,33 @@ blastp_result () {
     database_dir=/home/microbiome/data_storage/SATA1/database/transporterDB/blast_db/blast_db
     db="$database_dir/tcdb"
     
+    #extract the best matched subject and query
+    cat $result_dir/${genome_id}.tcdb.blastp.out |awk '!a[$1]++{print}' > $result_dir/${genome_id}.tcdb.blastp.best_matched.tab
     
-    $result_dir/${genome_id}.tcdb.blastp.out
+    #get best matched subject and query id
+    cat   $result_dir/${genome_id}.tcdb.blastp.best_matched.tab |awk '{print $1}' |sort -u > $result_dir/${genome_id}.tcdb.blastp.best_matched_query_id
     
+    #extract best matched query sequences
+    faSomeRecords $TMH  $result_dir/${genome_id}.tcdb.blastp.best_matched_query_id  $result_dir/${genome_id}.tcdb.blastp.best_matched_prot.fasta
+    sed -i 's/*//g' $result_dir/${genome_id}.tcdb.blastp.best_matched_prot.fasta
     
+    #extract the CDS sequences
+    cat $result_dir/${genome_id}.tcdb.blastp.best_matched_query_id |cut -d"|" -f 3 > $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl_id
+    for line in $(cat $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl_id); do
+        cat $result_dir/${genome_id}.CDS.fasta |grep -w $line|sed 's/>//g' >> $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl_query_id
+    done
     
+    faSomeRecords $result_dir/${genome_id}.CDS.fasta   $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl_query_id  $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl_query.fasta
+    sed -i 's/*//g'  $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl_query.fasta 
+    rm -f $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl_id  $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl_query_id
+    
+    perl -p -e 's/>jgi\|(\S+)\|(\d+)\|/>$1|$1_$2|$3/' $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl_query.fasta | sed 's/*//g'  >  $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl.fasta
+    perl -i -p -e 'unless( /^>/ ) {  s/x// }'  $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl.fasta
+    
+    rm -f $result_dir/${genome_id}.tcdb.blastp.best_matched_nucl_query.fasta 
    }
 
-export -f blastp_run
+export -f blastp_result
 
 Updated_genome_list=/home/microbiome/data_storage/SATA1/FunDB/Updated_genome_list
-time parallel -j 10 --eta --load 95% --noswap  blastp_run ::: $(cat $Updated_genome_list)
+time parallel -j 10 --eta --load 95% --noswap  blastp_result ::: $(cat $Updated_genome_list)
