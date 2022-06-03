a932fe7c08edfe43161a21f905129ece0f35c9fc
Haihua <58599570+HaihuaWang-hub@users.noreply.github.com>
2022-06-03 15:05:56 -0400

    Update 5.SeqID_collection.sh

--- database-generation/5.SeqID_collection.sh
+++ database-generation/5.SeqID_collection.sh
@@ -5,6 +5,24 @@
 FunDB_dir=/home/microbiome/data_storage/SATA1/Database
 
 echo -e "SeqID\tGenome_id\tGene_id\tprot_id\tTransporter_id\tTransporter_level1\tTransporter_level2\tTransporter_level3\tTransporter_level4\tTransporter_level5" > $FunDB_dir/FunDB_prot_seqID.tmp.tsv
+cat  $FunDB_dir/prot/$seq/*.prot2tcdb > $FunDB_dir/prot2tcdb
+cat  $FunDB_dir/prot2tcdb |while read line; do
+    SeqID=$(echo $line |awk '{print $1}' |cut -d "|" -f 2)
+    Genome_id=$(echo $line |awk '{print $1}' |cut -d "|" -f 1)
+    prot_id=$(echo $line |awk '{print $1}' |cut -d "|" -f 2 |cut -d"_" -f 2)
+    Gene_id=$(echo $line |awk '{print $1}' |cut -d "|" -f 3 |cut -d"_" -f 2,3,4,5,6,7,8,9,10,11,12)
+    Transporter_id=$(echo $line |awk '{print $2}')
+    Transporter_level1=$(echo $Transporter_id |cut -d"." -f 1)
+    Transporter_level2=$(echo $Transporter_id |cut -d"." -f 1,2)
+    Transporter_level3=$(echo $Transporter_id |cut -d"." -f 1,2,3)
+    Transporter_level4=$(echo $Transporter_id |cut -d"." -f 1,2,3,4)
+    Transporter_level5=$(echo $Transporter_id |cut -d"." -f 1,2,3,4,5)
+    echo -e "$SeqID\t$Genome_id\t$Gene_id\t$prot_id\t$Transporter_id\t$Transporter_level1\t$Transporter_level2\t$Transporter_level3\t$Transporter_level4\t$Transporter_level5" >> $FunDB_dir/FunDB_prot_seqID.tmp.tsv
+done
+mv -f $FunDB_dir/FunDB_prot_seqID.tmp.tsv $FunDB_dir/FunDB_prot_seqID.tsv
+
+
+
 for seq in $(ls $FunDB_dir/prot); do
     cat $FunDB_dir/prot/$seq |seqkit seq -n |while read line; do
          SeqID=$line
@@ -21,6 +39,18 @@ for seq in $(ls $FunDB_dir/prot); do
      done
 done
 mv -f $FunDB_dir/FunDB_prot_seqID.tmp.tsv $FunDB_dir/FunDB_prot_seqID.tsv
+
+
+
+
+
+
+
+
+
+
+
+
 
 echo -e "SeqID\tGenome_id\tGene_id\tprot_id\tTransporter_id\tTransporter_level1\tTransporter_level2\tTransporter_level3\tTransporter_level4\tTransporter_level5" > $FunDB_dir/FunDB_nucl_seqID.tmp.tsv
 for seq in $(ls $FunDB_dir/nucl); do
