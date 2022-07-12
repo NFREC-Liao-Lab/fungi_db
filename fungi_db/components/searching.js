a91d29aa5606aad3164504cebcb893a3e5608bcb
Simon Cole <simonccole1@gmail.com>
2022-07-12 16:24:13 -0400

    fixed more small errors before test vercel deployment

--- fungi_db/components/searching.js
+++ fungi_db/components/searching.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import mushroomIcon from "../public/mushroomIcon.png";
 
 
 export default function Searching(props){
